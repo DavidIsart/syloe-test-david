ee2e328ffe6cd3b1ad0b813f3e71bddd8f015fb6
DavidIsart <132366420+DavidIsart@users.noreply.github.com>
2023-09-27 15:58:25 +0200

    [Added] Video minute count text and slider

--- SyloeTT/Assets/SyloeTT/Scripts/UIVideo.cs
+++ SyloeTT/Assets/SyloeTT/Scripts/UIVideo.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -9,15 +11,37 @@ public class UIVideo : MonoBehaviour, IPointerClickHandler
 	[Header("References :")]
 	[SerializeField] private VideoPlayer _videoPlayer;
 	[SerializeField] private ToggleImage _toggleImage;
+	[SerializeField] private Slider _slider;
+	[SerializeField] private TextMeshProUGUI _timeText;
 	[SerializeField] private bool _paused = true;
 
-	private int fadeValue => _paused ? 1 : 0;
-
+	private float _videoLength;
 	private Coroutine _fadeCoroutine;
+
+	private int fadeValue => _paused ? 1 : 0;
+	private float clockTime => (float)_videoPlayer.clockTime;
+	private float watchPercentage => Mathf.Clamp01(clockTime / _videoLength);
 
 	void Awake()
 	{
 		_toggleImage.SetToggle(_paused);
+		_videoLength = (float)_videoPlayer.length;
+
+		UpdateSliderAndText();
+	}
+
+	void Update()
+	{
+		if (!_paused)
+			UpdateSliderAndText();
+	}
+
+	private void UpdateSliderAndText()
+	{
+		_slider.normalizedValue = watchPercentage;
+		_timeText.text = TimeSpan.FromSeconds(_videoPlayer.clockTime).ToString(@"mm\:ss");
+
+		print(TimeSpan.FromSeconds(_videoPlayer.clockTime).ToString(@"mm\:ss"));
 	}
 
 	public void OnPointerClick(PointerEventData eventData)
